01d39b72f5c6bdbcd288730e3e3221b23272b1a4
mohdzakir <mohdzakirr123@gmail.com>
2016-11-21 18:20:44 +0530

    added new line

--- Yeduniya.java
+++ Yeduniya.java
@@ -1,5 +1,6 @@
-publi class Yeduniya {
+public class Yeduniya {
 public static void main(Strings[] args) {
-System.out.println("this is another new java program")
+System.out.println("this is another new java program");
+System.out.println("Hello Zakir Bhai");
 }
 }
